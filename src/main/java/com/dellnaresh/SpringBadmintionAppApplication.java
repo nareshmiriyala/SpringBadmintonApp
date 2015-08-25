83c44e598a4acb92af1f477829dbbc2fea45cf93
Naresh Chandra Miriyala <ncmiriyala@gmail.com>
2015-08-25 23:16:47 +1000

    Commit the changes for Spring Badminton Accunt

---
+++ src/main/java/com/dellnaresh/SpringBadmintionAppApplication.java
@@ -0,0 +1,12 @@
+package com.dellnaresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBadmintionAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBadmintionAppApplication.class, args);
+    }
+}
---
+++ src/test/java/com/dellnaresh/SpringBadmintionAppApplicationTests.java
@@ -0,0 +1,18 @@
+package com.dellnaresh;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringBadmintionAppApplication.class)
+@WebAppConfiguration
+public class SpringBadmintionAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
